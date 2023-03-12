08784d517d0ef51e61dc2aa9016b18449322caab
= <=>
2023-03-13 00:59:01 +0100

    Updated a bunch, almost finished!

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,11 +27,18 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("com.aallam.openai:openai-client:3.1.0")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+}
+
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
     jvmToolchain(11)
 }
+
+
 
 // Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
--- src/main/kotlin/com/github/iodine98/intellijdocumentationplugin/docintention/DocumentationIntention.kt
+++ src/main/kotlin/com/github/iodine98/intellijdocumentationplugin/docintention/DocumentationIntention.kt
@@ -1,24 +1,89 @@
 package com.github.iodine98.intellijdocumentationplugin.docintention
 
+import com.aallam.openai.api.completion.CompletionRequest
+import com.aallam.openai.api.completion.TextCompletion
+import com.aallam.openai.api.model.ModelId
+import com.aallam.openai.client.OpenAI
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiMethod
+import com.intellij.refactoring.suggested.startOffset
+import io.github.cdimascio.dotenv.dotenv
+import kotlinx.coroutines.flow.Flow
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
-class DocumentationIntention : PsiElementBaseIntentionAction(), IntentionAction {
+
+open class DocumentationIntention : PsiElementBaseIntentionAction(), IntentionAction {
     override fun startInWriteAction(): Boolean = true
 
     override fun getText(): String = "Write documentation for this method"
 
     override fun getFamilyName(): String = "Write documentation"
 
+    private fun PsiElement.checkFunctionType(languageDisplayName: String): Boolean {
+        return when (languageDisplayName) {
+            "Java" -> this is PsiMethod
+            "Kotlin" -> this is KtNamedFunction
+            else -> true
+        }
+    }
+
+    private fun PsiElement.getNearestFunction(languageDisplayName: String): PsiElement? {
+        var parent = this.parent
+        while (parent != null) {
+            if (parent.checkFunctionType(languageDisplayName)) {
+                return parent
+            }
+            parent = parent.parent
+        }
+        return null
+    }
+
     override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
-        return element.parent is KtNamedFunction
+        val functionElement = element.getNearestFunction(element.language.displayName)
+        return functionElement?.checkFunctionType(element.language.displayName) ?: false
+    }
+
+    private fun getCompletionResponseFromOpenAI(
+        functionContent: String,
+        languageDisplayName: String
+    ): Flow<TextCompletion> {
+        val dotenv = dotenv()
+        val envVar: String = dotenv["OPENAI_API_KEY"]
+        val openAIClient = OpenAI(envVar)
+        val documentationLanguage = when (languageDisplayName) {
+            "Java" -> "JavaDoc"
+            "Kotlin" -> "KDoc"
+            else -> "documentation"
+        }
+        val completionRequest = CompletionRequest(
+            model = ModelId("code-cushman-001"),
+            prompt = "Return only the" + documentationLanguage + "for the function below that has been written in " +
+                    languageDisplayName +
+                    "\n " +
+                    "```" + languageDisplayName + "\n" +
+                    functionContent + "\n" +
+                    "```"
+        )
+        return openAIClient.completions(completionRequest)
     }
 
     override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
-        TODO("Not yet implemented")
+        val methodContext = element.getNearestFunction(element.language.displayName)
+        if (editor != null && methodContext != null && methodContext.checkFunctionType(element.language.displayName)) {
+//            val openAICompletionsFlow =
+//                getCompletionResponseFromOpenAI(methodContext.text, element.language.displayName)
+            editor.caretModel.moveToOffset(methodContext.startOffset)
+//            val document = editor.document
+
+//            openAICompletionsFlow.map { it -> WriteCommandAction.runWriteCommandAction(project, () -> {
+//                document.re
+//            }) } }
+            TODO("Not yet implemented")
+        }
+
     }
 }
