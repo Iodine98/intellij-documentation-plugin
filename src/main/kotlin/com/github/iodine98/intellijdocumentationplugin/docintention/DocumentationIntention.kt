1c99f37a1429efda66137fccda96f52afec4c108
= <=>
2023-03-12 22:14:29 +0100

    Added Java and Kotlin SDK to import platform plugins

--- src/main/kotlin/com/github/iodine98/intellijdocumentationplugin/docintention/DocumentationIntention.kt
+++ src/main/kotlin/com/github/iodine98/intellijdocumentationplugin/docintention/DocumentationIntention.kt
@@ -5,6 +5,7 @@ import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import com.intellij.psi.util.elementType
 
 class DocumentationIntention : PsiElementBaseIntentionAction(), IntentionAction {
@@ -15,8 +16,7 @@ class DocumentationIntention : PsiElementBaseIntentionAction(), IntentionAction 
     override fun getFamilyName(): String = "Write documentation"
 
     override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
-        val elementType = element.parent.elementType
-        return false
+        return element is KtNamedFunction
     }
 
     override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
